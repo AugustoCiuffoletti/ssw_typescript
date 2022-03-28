f1054823cbe13ada4d7ecc2739120e0c1abee801
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-28 11:18:55 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
-class miaClasse {
+interface miaInterfaccia {
+  f1: string;
+  f2: number;
+}
+class miaClasse implements miaInterfaccia {
     f1: string;
     f2: number;
+    sum: ((a:number,b:number) => number) = function(x,y) {return x+y};
     constructor( a: string, b: number ) {
         this.f1=a,
         this.f2=b
     }
 };
-var A: Array<miaClasse> = []; // anche miaClasse[]
+var A: Array<miaInterfaccia> = [];
 var n: number;
-for (n = 0; n < 3 ; n++ ) {
-    A[n] = new miaClasse("",n) ;
-}
+A[0] = {f1: "nome1",f2: 6};
+A[1] = new miaClasse("nome2",12);
 console.log(A)
